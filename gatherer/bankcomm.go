43abb8075d563a595e580ddff92cc998963d6630
Mike Shi <mikeshi80@gmail.com>
2013-06-21 23:10:26 +0800

    尝试读取交通银行信息

--- gatherer/bankcomm.go
+++ gatherer/bankcomm.go
@@ -4,9 +4,11 @@ package gatherer
 The data gatherer for Bank COMM 交通银行
 */
 import (
+	"net/url"
+    "strings"
 	ds "creditcardnav/data"
 	"github.com/PuerkitoBio/goquery"
-	"net/url"
+    "code.google.com/p/go.net/html"
 )
 
 type BankCOMM struct {
@@ -15,16 +17,16 @@ type BankCOMM struct {
 
 func (this *BankCOMM) GenerateURLs() (urls []string, err error) {
 	values := make(url.Values, 10)
-	values["cityId"] = "2c92de8f2ee5d079012ee5d50dda0003"
-	values["cityName"] = "上海"
-	values["circleId"] = "0"
-	values["catalog"] = "0"
-	values["cardType"] = ""
-	values["keyWord"] = ""
-	values["pageSize"] = "20"
-	values["pageNo"] = "1"
-	values["orderBy"] = "0"
-	values["isPage"] = "true"
+	values.Set("cityId", "2c92de8f2ee5d079012ee5d50dda0003")
+	values.Set("cityName", "上海")
+	values.Set("circleId", "0")
+	values.Set("catalog", "0")
+	values.Set("cardType", "")
+	values.Set("keyWord", "")
+	values.Set("pageSize", "20")
+	values.Set("pageNo", "1")
+	values.Set("orderBy", "0")
+	values.Set("isPage", "true")
 
 	body, err := this.FetchTextByPost("http://creditcard.bankcomm.com/bcms/front/merchant/ajax/search.do", values)
 
@@ -32,7 +34,18 @@ func (this *BankCOMM) GenerateURLs() (urls []string, err error) {
 		return nil, err
 	}
 
-	return body, err
+	//return body, err
+    node, err := html.Parse(strings.NewReader(body))
+    doc := goquery.NewDocumentFromNode(node)
+
+    urls = make([]string, 20)
+    doc.Find("div.ml-pic>a[target=_blank]").Each(func(i int, s *goquery.Selection) {
+        attr, exists := s.Attr("href")
+        if exists {
+            urls = append(urls, "http://creditcard.bankcomm.com/bcms/info1/" + attr)
+        }
+    })
+    return urls, nil
 }
 
 func (this *BankCOMM) ClawerList() (events []ds.Event, err error) {
